0c9be181144046e1c633847b65b206b3e6bca0e4
Nikolas dos Santos <58566087+Nikolassantos@users.noreply.github.com>
2020-03-03 22:31:24 -0300

    Emplementando animção de entrada
    modificação no Routes

--- src/routes.js
+++ src/routes.js
@@ -1,39 +1,55 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Home from './pages/Home';
+import React, { useEffect, useState } from 'react'
+import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
+import HomeScreen from './pages/Home'
+import LoadingScreen from './pages/loading/'
 
-const Stack = createStackNavigator();
-const bgHeaderColor = "#EE2121";
-const txtHeaderColor = "#fff"
+const Stack = createStackNavigator()
+const bgHeaderColor = '#EE2121'
+const txtHeaderColor = '#fff'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  })
+
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle:{
-            backgroundColor: bgHeaderColor
-          },
-          headerTitleAlign: 'center',
-          headerTintColor: txtHeaderColor,
-          headerTitleStyle: {
-            fontWeight: 'bold'
-          }
-        }}
-      >
-        <Stack.Screen options={({navigation}) => ({
-          title: 'Coffee Break'
-        })} 
-        name="Home" 
-        component={Home} />
-      </Stack.Navigator>
-
+      {loading ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Splash" component={LoadingScreen} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: bgHeaderColor
+            },
+            headerTitleAlign: 'center',
+            headerTintColor: txtHeaderColor,
+            headerTitleStyle: {
+              fontWeight: 'bold'
+            }
+          }}
+        >
+          <Stack.Screen
+            options={({ navigation }) => ({
+              title: 'Coffee Break'
+            })}
+            name="Home"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
-  );
+  )
 }
 
-export default App;
+export default App
